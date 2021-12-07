984913ca2913e4fcd38e97cb3a923ba5bb86e7cf
kogawakuchitomoaki <kogawakuchi1240@gmail.com>
2021-12-07 09:19:33 +0900

    課題1終了

---
+++ chap1_2.php
@@ -0,0 +1,9 @@
+<?php
+var_dump("上底は？");
+$top = trim(fgets(STDIN));
+var_dump("下底は？");
+$bottom = trim(fgets(STDIN));
+var_dump("高さは？");
+$height = trim(fgets(STDIN));
+$msg = trim(($top+$bottom)*$height/2);
+var_dump("面積は" . $msg . "です。");
